d6b032fc9dfa5b666a3096d9ca30a7f299b13942
alvaro61017 <127184264+alvaro61017@users.noreply.github.com>
2025-05-05 16:15:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -282,7 +282,7 @@ if df is not None:
                 sin_ganar_seguidos.append((equipo, no_ganar))
 
             sin_ganar_seguidos = sorted(sin_ganar_seguidos, key=lambda x: x[1], reverse=True)[:5]
-            st.dataframe(pd.DataFrame(sin_ganar_seguidos, columns=['Equipo', 'Racha de Partidos sin Ganar']).style.hide_index(), use_container_width=True)
+            st.dataframe(pd.DataFrame(sin_ganar_seguidos, columns=['Equipo', 'Racha de Partidos sin Ganar']).reset_index(drop=True), use_container_width=True)
 
 
         st.header("🎯 Goleadores")
