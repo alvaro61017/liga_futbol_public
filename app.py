fef918cd2d573e92995ff327bdf7e23fa97fd309
alvaro61017 <127184264+alvaro61017@users.noreply.github.com>
2025-05-30 12:39:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ if not st.session_state.get("initialized", False):
     st.session_state["categoria_final"] = seleccion
     st.session_state["initialized"] = True
 
-    st.experimental_rerun()
+    st.rerun()
 
 # 2) Layout principal
 categoria = st.session_state["categoria_final"]
