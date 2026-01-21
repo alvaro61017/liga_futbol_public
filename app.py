89e71f8b00dd9c2a92981703cd7681ab948e2a54
alvaro61017 <127184264+alvaro61017@users.noreply.github.com>
2026-01-21 15:59:08 +0100

    csv jornada 16

--- app.py
+++ app.py
@@ -48,8 +48,9 @@ CATEGORIAS = {
     # "Senior 25/26": "1tBRXPiviwLNDeuHafzGuiYiqxH0v2fkz", # jornada 6
     # "Senior 25/26": "1ZbovOIx3eoTJ0CYz52uNrrehDRTntmU4", # jornada 8
     # "Senior 25/26": "1Z6Cr9d-oQoxQFfWsM0w9WBc3TCA7aguK", # jornada 9
-    "Senior 25/26": "1B2EbOyZ9PJqmzhEBCYLH8eF4vzvS9dE9", # jornada 11
-    "Senior 25/26": "15HYDdWA4Fpxm0ThHkzyZSy1quAug5TP1",
+    # "Senior 25/26": "1B2EbOyZ9PJqmzhEBCYLH8eF4vzvS9dE9", # jornada 11
+    # "Senior 25/26": "15HYDdWA4Fpxm0ThHkzyZSy1quAug5TP1", Jornada 15
+    "Senior 25/26": "1m4VieqnlyMDv3M0Azj5GuAoq1oqNa5XM", # Jornada 16
     "Senior 24/25": "1am5lxSOlqlBP1R6sic9_T4a4mBF4pL2i", 
     "Senior 23/24": "1ji4IPhKoYJHg25PS--oGCTMDVJ-laksb",
     "Senior 22/23": "1phka39s5gjuCbnIKnNheHgIaU6CvK5CL",
@@ -1794,6 +1795,7 @@ if df is not None:
 
 else:
     st.warning("❌ No se pudieron cargar los datos desde Google Drive.")
+
 
 
 
